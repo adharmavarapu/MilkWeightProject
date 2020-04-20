c9ca0047b7ba48bd07885d045fc81f9c9d89703f
akudkilwar <kudkilwar@wisc.edu>
2020-04-19 23:19:19 -0500

    Added layout for Update Data window - AK

--- application/Main.java
+++ application/Main.java
@@ -1,10 +1,8 @@
 package MilkWeightProject.application;
 
-import java.util.Date;
 import java.util.List;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -17,24 +15,26 @@ import javafx.stage.Stage;
 
 /**
  * The graphical user interface of the Milk Weight program
+ * 
  * @author Abhilash Dharmavarapu, Atharva Kudkilwar
  *
  */
-public class Main extends Application{
+public class Main extends Application {
 	private List<String> args;
 	private static final String APP_TITLE = "Milk Weights";
 
 	/**
 	 * Method that runs show primary Stage
-	 * @param primaryStage Stage passed down in launch of application 
+	 * 
+	 * @param primaryStage Stage passed down in launch of application
 	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		args = this.getParameters().getRaw();
 		BorderPane root = new BorderPane();
-		
-		//Set left side of the main screen
+
+		// Set left side of the main screen
 		VBox leftButtons = new VBox();
 		Button newData = new Button("", new Label("Add New Data"));
 		newData.setOnAction(e -> newDataWindow(primaryStage));
@@ -43,13 +43,13 @@ public class Main extends Application{
 		leftButtons.getChildren().addAll(newData, updateData);
 		leftButtons.setSpacing(15);
 		root.setLeft(leftButtons);
-		
-		//Set Right side of the main screen
+
+		// Set Right side of the main screen
 		Button weightDifference = new Button("", new Label("Update Data"));
 		weightDifference.setOnAction(e -> weightDifferenceWindow(primaryStage));
-		root.setRight(weightDifference);		
-		
-		//Finalize and set stage to main scene
+		root.setRight(weightDifference);
+
+		// Finalize and set stage to main scene
 		Scene mainScene = new Scene(root);
 		primaryStage.setWidth(400);
 		primaryStage.setHeight(150);
@@ -57,69 +57,107 @@ public class Main extends Application{
 		primaryStage.setScene(mainScene);
 		primaryStage.show();
 	}
+
 	private void weightDifferenceWindow(Stage primaryStage) {
-		//CREATE WINDOW FOR WEIGHT DIFFERENCE
+		// CREATE WINDOW FOR WEIGHT DIFFERENCE
 		Stage wD = new Stage();
 		wD.setTitle("Weight Difference");
 		wD.initModality(Modality.APPLICATION_MODAL);
 		wD.initOwner(primaryStage);
 		wD.show();
 	}
+
 	private void updateDataWindow(Stage primaryStage) {
-		//CREATE WINDOW FOR UPDATE DATA
+		// CREATE WINDOW FOR UPDATE DATA
 		Stage uD = new Stage();
 		uD.setTitle("Update Existing Data");
 		uD.initModality(Modality.APPLICATION_MODAL);
 		uD.initOwner(primaryStage);
-		uD.show();
-	}
-	public void newDataWindow(Stage primaryStage) {
-		//CREATE WINDOW FOR NEW DATA
-		Stage nD = new Stage();
-		nD.setTitle("Add New Data");
-		nD.initModality(Modality.APPLICATION_MODAL);
-		nD.initOwner(primaryStage);
-		
-		
+
 		BorderPane root = new BorderPane();
+
+		HBox hb1 = new HBox(); // HBox for Previous Date line
+		TextField oldDate = new TextField();
+		oldDate.setPromptText("yyyy-mm-dd");
+		hb1.getChildren().addAll(new Label("Enter Previous Date: "), oldDate);
+
+		HBox hb2 = new HBox(); // HBox for New Date line
+		TextField newDate = new TextField();
+		newDate.setPromptText("yyyy-mm-dd");
+		hb2.getChildren().addAll(new Label("Enter New Date: "), newDate);
 		
-		HBox hb1 = new HBox(); // HBox for farm ID line 
-		TextField farmID = new TextField();
-		farmID.setPromptText("Farm ID");
-		hb1.getChildren().addAll(new Label("Enter Farm ID: "), farmID);
+		HBox hb3 = new HBox(); // HBox for Old Weight
+		TextField oldWeight = new TextField();
+		oldWeight.setPromptText("Enter Weight");
+		hb3.getChildren().addAll(new Label("Enter Previous Weight: "), oldWeight);
 		
-		HBox hb2 = new HBox(); // HBox for Date line
-		TextField date = new TextField();
-		date.setPromptText("yyyy-mm-dd");
-		hb2.getChildren().addAll(new Label("Enter Date: "), date);
-		
-		HBox hb3 = new HBox(); // HBox for Weight line
-		TextField weight = new TextField();
-		weight.setPromptText("Enter Weight");
-		hb3.getChildren().addAll(new Label("Enter Milk Weight: "), weight);
+		HBox hb4 = new HBox();
+		TextField newWeight = new TextField(); // HBox for new Weight
+		newWeight.setPromptText("Enter Weight: ");
+		hb4.getChildren().addAll(new Label("Enter New Weight: "), newWeight);
 		
 		Button bt = new Button("DONE"); // Done Button
 		
-		VBox vb = new VBox(); // VBox to put all 4 elements together
-		vb.getChildren().addAll(hb1, hb2, hb3, bt);
+		VBox vb = new VBox();
+		vb.getChildren().addAll(hb1, hb2, hb3, hb4, bt);
 		vb.setSpacing(25);
 		
 		root.setCenter(vb);
 		
+		Scene sc = new Scene(root);
+		uD.setScene(sc);
+		uD.setHeight(500);
+		uD.setWidth(350);
+		uD.show();
+	}
+
+	public void newDataWindow(Stage primaryStage) {
+		// CREATE WINDOW FOR NEW DATA
+		Stage nD = new Stage();
+		nD.setTitle("Add New Data");
+		nD.initModality(Modality.APPLICATION_MODAL);
+		nD.initOwner(primaryStage);
+
+		BorderPane root = new BorderPane();
+
+		HBox hb1 = new HBox(); // HBox for farm ID line
+		TextField farmID = new TextField();
+		farmID.setPromptText("Farm ID");
+		hb1.getChildren().addAll(new Label("Enter Farm ID: "), farmID);
+
+		HBox hb2 = new HBox(); // HBox for Date line
+		TextField date = new TextField();
+		date.setPromptText("yyyy-mm-dd");
+		hb2.getChildren().addAll(new Label("Enter Date: "), date);
+
+		HBox hb3 = new HBox(); // HBox for Weight line
+		TextField weight = new TextField();
+		weight.setPromptText("Enter Weight");
+		hb3.getChildren().addAll(new Label("Enter Milk Weight: "), weight);
+
+		Button bt = new Button("DONE"); // Done Button
+
+		VBox vb = new VBox(); // VBox to put all 4 elements together
+		vb.getChildren().addAll(hb1, hb2, hb3, bt);
+		vb.setSpacing(25);
+
+		root.setCenter(vb);
+
 		Scene scene = new Scene(root);
 		nD.setScene(scene);
 		nD.setWidth(500);
 		nD.setHeight(350);
 		nD.show();
 	}
+
 	/**
 	 * Method that will run upon program execution
+	 * 
 	 * @param args System arguments to pass down before running
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch(args);
 	}
-
 
 }

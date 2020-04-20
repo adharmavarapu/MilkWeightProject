823a6f6c38d565551d93389b68bc8b350fd9bf3b
akudkilwar <kudkilwar@wisc.edu>
2020-04-19 22:56:58 -0500

    Added layout for Add New Data window - AK

--- application/Main.java
+++ application/Main.java
@@ -1,5 +1,6 @@
-package application;
+package MilkWeightProject.application;
 
+import java.util.Date;
 import java.util.List;
 
 import javafx.application.Application;
@@ -7,14 +8,16 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
  * The graphical user interface of the Milk Weight program
- * @author Abhilash Dharmavarapu, 
+ * @author Abhilash Dharmavarapu, Atharva Kudkilwar
  *
  */
 public class Main extends Application{
@@ -35,7 +38,7 @@ public class Main extends Application{
 		VBox leftButtons = new VBox();
 		Button newData = new Button("", new Label("Add New Data"));
 		newData.setOnAction(e -> newDataWindow(primaryStage));
-		Button updateData = new Button("", new Label("Add New Data"));
+		Button updateData = new Button("", new Label("Update Data"));
 		updateData.setOnAction(e -> updateDataWindow(primaryStage));
 		leftButtons.getChildren().addAll(newData, updateData);
 		leftButtons.setSpacing(15);
@@ -76,6 +79,37 @@ public class Main extends Application{
 		nD.setTitle("Add New Data");
 		nD.initModality(Modality.APPLICATION_MODAL);
 		nD.initOwner(primaryStage);
+		
+		
+		BorderPane root = new BorderPane();
+		
+		HBox hb1 = new HBox(); // HBox for farm ID line 
+		TextField farmID = new TextField();
+		farmID.setPromptText("Farm ID");
+		hb1.getChildren().addAll(new Label("Enter Farm ID: "), farmID);
+		
+		HBox hb2 = new HBox(); // HBox for Date line
+		TextField date = new TextField();
+		date.setPromptText("yyyy-mm-dd");
+		hb2.getChildren().addAll(new Label("Enter Date: "), date);
+		
+		HBox hb3 = new HBox(); // HBox for Weight line
+		TextField weight = new TextField();
+		weight.setPromptText("Enter Weight");
+		hb3.getChildren().addAll(new Label("Enter Milk Weight: "), weight);
+		
+		Button bt = new Button("DONE"); // Done Button
+		
+		VBox vb = new VBox(); // VBox to put all 4 elements together
+		vb.getChildren().addAll(hb1, hb2, hb3, bt);
+		vb.setSpacing(25);
+		
+		root.setCenter(vb);
+		
+		Scene scene = new Scene(root);
+		nD.setScene(scene);
+		nD.setWidth(500);
+		nD.setHeight(350);
 		nD.show();
 	}
 	/**
